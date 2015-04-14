d0e8c38e99a023b657a95ccd2477a91b147948a5
jdebeer <a.j.debeer@gmail.com>
2015-04-14 00:47:43 -0400

    feat(flashcards): multiple flashcards with color selection and angular filters for sorting and capitalization

--- public/app.js
+++ public/app.js
@@ -1,1 +1,65 @@
 var app = angular.module('FlashCards', []);
+
+
+app.controller('MainController', function ($scope) {
+
+  $scope.flashCards = [
+    {
+        question: 'What is Angular?',
+        answers: [
+            { text: 'A front-end framework for great power!', correct: true },
+            { text: 'Something lame, who cares, whatever.', correct: false },
+            { text: 'Some kind of fish, right?', correct: false }
+        ],
+        answered: false,
+        answeredCorrectly: false
+    },
+    {
+        question: 'What is a controller?',
+        answers: [
+            { text: 'Something that manages my front-end routes', correct: false },
+            { text: 'A function that allows me to manage a scope', correct: true },
+            { text: 'An Angular template', correct: false }
+        ],
+        answered: false,
+        answeredCorrectly: false
+
+    },
+    {
+        question: 'What does {{ }} do?',
+        answers: [
+            { text: 'It runs some Javascript', correct: false },
+            { text: 'It looks for variables in HTML', correct: false },
+            { text: 'It runs an Angular expression that accesses my $scope', correct: true }
+        ],
+        answered: false,
+        answeredCorrectly: false
+    }
+  ];
+
+});
+
+app.controller('FlashCardController', function ($scope) {
+  // $scope.flashCard = {
+  //   question: 'What is Angular?',
+  //   answers: [
+  //       { text: 'A front-end framework for great power!', correct: true },
+  //       { text: 'Something lame, who cares, whatever.', correct: false },
+  //       { text: 'Some kind of fish, right?', correct: false }
+  //   ], answered: false
+  // };
+
+  $scope.answerQuestion = function (flashcard, answer) {
+    console.log('nahhh');
+    if (flashcard.answered === false) {
+      console.log('waahhh');
+      flashcard.answered = true;
+      if ( answer.correct === true ) {
+        flashcard.answeredCorrectly = true;
+      } else {
+        flashcard.answeredCorrectly = false;
+      }
+    }
+  };
+
+});
